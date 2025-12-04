08c4d196f6cafcd5577a07d1ecaf324d37ae18c8
OmarQasoma <o.qasoma@gmail.com>
2025-12-04 14:37:41 +0100

    Add second activity and navigation from main screeen

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.firstapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // نجيب الزر من الواجهة
+        val btnStartGame: Button = findViewById(R.id.btnStartGame)
+
+        // لما نضغط الزر، نفتح GameActivity
+        btnStartGame.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
